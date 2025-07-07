cb9f02979fae0031d1592581cdd899f70639b702
Boda Rajesh <154888255+RajeshBodatech@users.noreply.github.com>
2025-07-07 21:40:06 +0530

    Update App.jsx
    updated link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -507,7 +507,7 @@ function ContactForm() {
     }
     setLoading(true);
     try {
-      const res = await fetch('http://localhost:5000/api/contact', {
+      const res = await fetch(' https://portfolio-qj8m.onrender.com/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
